5bd1720d65a26d52f211100e67bbdfcea093e524
AviaBy <106016664+AviaBy@users.noreply.github.com>
2022-05-22 14:50:37 +0300

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -74,9 +74,9 @@
  * Then you can verify voltage on debug output value 6 (to get calibrated voltage multiplied by 100).
 */
 #define BAT_FILT_COEF           655       // battery voltage filter coefficient in fixed-point. coef_fixedPoint = coef_floatingPoint * 2^16. In this case 655 = 0.01 * 2^16
-#define BAT_CALIB_REAL_VOLTAGE  4300      // input voltage measured by multimeter (multiplied by 100). In this case 43.00 V * 100 = 4300
-#define BAT_CALIB_ADC           1704      // adc-value measured by mainboard (value nr 5 on UART debug output)
-#define BAT_CELLS               12        // battery number of cells. Normal Hoverboard battery: 10s
+#define BAT_CALIB_REAL_VOLTAGE  3970      // input voltage measured by multimeter (multiplied by 100). In this case 43.00 V * 100 = 4300
+#define BAT_CALIB_ADC           1492      // adc-value measured by mainboard (value nr 5 on UART debug output)
+#define BAT_CELLS               10        // battery number of cells. Normal Hoverboard battery: 10s
 #define BAT_LVL2_ENABLE         0         // to beep or not to beep, 1 or 0
 #define BAT_LVL1_ENABLE         1         // to beep or not to beep, 1 or 0
 #define BAT_BLINK_INTERVAL      80        // battery led blink interval (80 loops * 5ms ~= 400ms)
@@ -330,29 +330,30 @@
     #define FLASH_WRITE_KEY     0x1001    // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
     #define DEBUG_SERIAL_USART3           // right sensor board cable, disable if I2C (nunchuk or lcd) is used!
   #endif
-
+  #define INVERT_R_DIRECTION
+  #define INVERT_L_DIRECTION
   // #define ADC_ALTERNATE_CONNECT           // use to swap ADC inputs (left and right. best practise is to swap cables and to use the same config on both boards.)
 
   // Here you can tune the speeds and accelerations of the 4 modes:
-  #define MAX_SPEED_FORWARDS_M1 130  // speed_calculator.ods
-  #define ACC_FORWARDS_M1 0.8
-  #define MAX_SPEED_BACKWARDS_M1 90
-  #define ACC_BACKWARDS_M1 0.8
+  #define MAX_SPEED_FORWARDS_M1 200  // speed_calculator.ods
+  #define ACC_FORWARDS_M1 1.0
+  #define MAX_SPEED_BACKWARDS_M1 150
+  #define ACC_BACKWARDS_M1 1.0
 
-  #define MAX_SPEED_FORWARDS_M2 280
-  #define ACC_FORWARDS_M2 0.8
-  #define MAX_SPEED_BACKWARDS_M2 190
-  #define ACC_BACKWARDS_M2 0.8
+  #define MAX_SPEED_FORWARDS_M2 400
+  #define ACC_FORWARDS_M2 2.0
+  #define MAX_SPEED_BACKWARDS_M2 180
+  #define ACC_BACKWARDS_M2 2.0
 
-  #define MAX_SPEED_FORWARDS_M3 540
-  #define ACC_FORWARDS_M3 0.8
-  #define MAX_SPEED_BACKWARDS_M3 240
-  #define ACC_BACKWARDS_M3 0.8
+  #define MAX_SPEED_FORWARDS_M3 600
+  #define ACC_FORWARDS_M3 4.0
+  #define MAX_SPEED_BACKWARDS_M3 220
+  #define ACC_BACKWARDS_M3 4.0
 
   #define MAX_SPEED_FORWARDS_M4 1000  // do not change this in mode 4!
-  #define ACC_FORWARDS_M4 0.8
+  #define ACC_FORWARDS_M4 5.0
   #define MAX_SPEED_BACKWARDS_M4 300
-  #define ACC_BACKWARDS_M4 0.8
+  #define ACC_BACKWARDS_M4 5.0
 
   #define DEBUG_SERIAL_USART3       // right sensor cable debug
 #endif
